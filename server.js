e15eff62295e58d3cfc517bf2d1cd34548333e68
jfarris <jfarris@berkeley.edu>
2015-12-16 15:43:24 -0800

    added underscore.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
+
 var app = express();
 var PORT = process.env.PORT || 3000;
-
 var todos = [];
 var todoNextId = 1;
 
@@ -20,13 +21,15 @@ app.get('/todos', function (req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function (req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matched;
 
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matched = todo;
-		}
-	});
+	var matched = _.findWhere(todos, {id: todoId})
+	// var matched;
+
+	// todos.forEach(function (todo) {
+	// 	if (todoId === todo.id) {
+	// 		matched = todo;
+	// 	}
+	// });
 
 	if (matched) { 
 		res.json(matched);
@@ -37,12 +40,18 @@ app.get('/todos/:id', function (req, res) {
 
 // POST /todos
 app.post('/todos', function (req, res) {
-	var body = req.body;
+	var body = _.pick(req.body,'description', 'completed');
+
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)  {
+		return res.status(400).send();
+	}
+
 	req.body.id = todoNextId++;
 
-	todos.push(req.body)
+	body.description = body.description.trim();
+	todos.push(body)
 	
-	console.log('description: ' + req.body.description);
+	console.log('description: ' + body.description);
 	res.json(body);
 });
 
