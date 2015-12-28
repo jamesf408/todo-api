40df271877fa51a88658512b9ebbcaa941010f71
jfarris <jfarris@berkeley.edu>
2015-12-28 12:45:41 -0800

    convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -16,26 +16,28 @@ app.get('/', function(req, res) {
 
 // GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
-	var queryParams = req.query;
-	var filteredTodos = todos;
+	var query = req.query;
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
+	} 
+
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: "%" + query.q + "%"
+		}
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
-	}
-
-	res.json(filteredTodos);
+	db.todo.findAll({where: where}).then(function (todos) {
+		res.json(todos);
+	},
+	function (e) {
+		res.status(500).send();
+	});
 });
 
 // GET /todos/:id
@@ -52,12 +54,6 @@ app.get('/todos/:id', function(req, res) {
 	}, function (e) {
 		res.status(500).json(e);
 	});
-
-	// if (matchedTodo) {
-	// 	res.json(matchedTodo);
-	// } else {
-	// 	res.status(404).send();
-	// }
 });
 
 // POST /todos
@@ -69,20 +65,6 @@ app.post('/todos', function(req, res) {
 	}, function (e) {
 		res.status(400).json(e);
 	});
-	// call create on db.todo
-	//   respond with 200 and todo
-	//   res.status(400).json(e)
-
-	// if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-	// 	return res.status(400).send();
-	// }
-
-	// body.description = body.description.trim();
-	// body.id = todoNextId++;
-
-	// todos.push(body);
-
-	// res.json(body);
 });
 
 // DELETE /todos/:id
