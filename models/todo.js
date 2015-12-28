af65dbb063a2c66a7827823c2d71d20d33dc0a1f
jfarris <jfarris@berkeley.edu>
2015-12-28 15:00:51 -0800

    fixed todo.js to validate boolean for completed.  updated server.js PUT todos/1

--- models/todo.js
+++ models/todo.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) { 
 	return sequelize.define('todo', {
 		description: {
 			type: DataTypes.STRING,
@@ -10,7 +10,14 @@ module.exports = function(sequelize, DataTypes) {
 		completed: {
 			type: DataTypes.BOOLEAN,
 			allowNull: false,
-			defaultValue: false
+			defaultValue: false,
+			validate: {
+				isBoolean: function (value) {
+					if (typeof value !== 'boolean') {
+						throw new Error('Completed must be a boolean');
+					}
+				}
+			}
 		}
 	});
 };
